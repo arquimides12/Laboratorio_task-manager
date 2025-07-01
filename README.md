df8ed8935008c54d17ecb3d5dd7ab6b9e94f3c50
arquimides12 <camposanderson323@gmail.com>
2025-07-01 15:59:10 -0500

    node_module

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Laboratorio_task-manager
 Laboratorio de la Unidad 2 materia programación integrativa 
+
+# Instalacion de litElement 
+
+```bash 
+npm init
+npm install lit-element
+npm install webpack webpack-cli --save-dev
+npm install webpack webpack-dev-server --save-dev
+npm run serve
+npm install --save-dev html-webpack-plugin
+```
+
+## pedro
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',  // O el archivo donde inicia tu aplicación
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.html']
+  }
+};
